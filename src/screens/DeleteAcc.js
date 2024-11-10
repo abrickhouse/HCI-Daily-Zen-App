5b67efb820c14afefa01ec5e73c3f175e6e8ab70
aamaiyaa <74420296+aamaiyaa@users.noreply.github.com>
2024-11-09 23:37:58 -0500

    del screen edit

--- src/screens/DeleteAcc.js
+++ src/screens/DeleteAcc.js
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 function DeleteAcc() {
  return (
   <div className="del-screen p-5 text-center">
-   <div className="mid-txt">
-    <h1 className="mainheader py-4">We're sad to see you go!</h1>
-    <h5>
-     You're always welcome to join again... but you can still play games!
-    </h5>
-    <Link to={`/home`}>
-     <button class="btn btn-light btnx px-4 m-3 btn-lg">Go Back Home</button>
-    </Link>
-   </div>
+   <h1 className="mainheader py-4">We're sad to see you go!</h1>
+   <h5 className="wide">
+    You're always welcome to join again... but you can still play games!
+   </h5>
+   <Link to={`/home`}>
+    <button class="btn btn-light btnx px-4 m-3 btn-lg">Go Back Home</button>
+   </Link>
   </div>
  );
 }
