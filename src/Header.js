9c09f982d75cc5c2577268f15eb772ce883d1de7
aamaiyaa <74420296+aamaiyaa@users.noreply.github.com>
2024-11-12 15:19:57 -0500

    header

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,9 @@ function Header() {
  return (
   <div className="d-flex justify-content-center sticky-top nav-header py-2">
    {name ? (
-    <nav class="navbar navbar-light nav-head ">
+    <nav class="navbar navbar-light nav-head d-block">
      <Link to={`/home/${name}`}>
-      <img src={logo} className="app-logo" alt="logo" />
+      <img src={logo} className="app-logo mx-auto d-block" alt="logo" />
      </Link>
     </nav>
    ) : (
